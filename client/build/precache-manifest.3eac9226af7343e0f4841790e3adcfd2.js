57dbcdea0a922f1d34365c369a71a410569b453b
Lissette <lisssettemvega@gmail.com>
2022-01-28 19:48:41 -0500

    initialize files and more

---
+++ client/build/precache-manifest.3eac9226af7343e0f4841790e3adcfd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65989f9b7ad6decb04958415c9efbc44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0547326d3494e218bb9e",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "41ff8229b0844499817f",
+    "url": "/static/js/2.9c69decb.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.9c69decb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0547326d3494e218bb9e",
+    "url": "/static/js/main.9fcc9d4c.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
